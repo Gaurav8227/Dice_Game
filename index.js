de3e661dbd1e1d038290c86d4c65d0ac5150af4d
Gaurav8227 <85555649+Gaurav8227@users.noreply.github.com>
2021-06-18 10:09:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 var random1 = Math.floor(Math.random()*6)+1;
 var rimage = "dice" + random1 + ".png";
-var rsource = "images/" + rimage ;
+var rsource = rimage ;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",rsource);
 
 
 var random2 = Math.floor(Math.random()*6)+1;
 var rimage2 = "dice" + random2 + ".png";
-var rsource2 = "images/" + rimage2 ;
+var rsource2 = rimage2 ;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",rsource2);
 
